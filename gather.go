7f7278e311333ea4c8c49466e19545964fc2d61e
Matt Pladna <pladdypants@gmail.com>
2016-10-05 23:47:07 -0400

    moved a lot of things; made more generic

--- gather.go
+++ gather.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"regexp"
@@ -21,24 +23,91 @@ func main() {
 		lumberjack.Fatal("Config file is required as an argument")
 	}
 
-	// Read in config
-	configContents := ReadConfig(os.Args[1])
-	config := UnmarshalConfig(
-		ReplaceTime(configContents, timepiece.TimeToTimePiece(time.Now())))
+	config := readConfig(os.Args[1], time.Now())
+	var filesToDownload []string
 
+	if config["file list host"] != nil {
+		matchingFiles, err := getFileList(config)
+		if err != nil {
+			lumberjack.Fatal("Failed to get file list")
+		}
+
+		if len(matchingFiles) == 0 {
+			lumberjack.Info("No files found to download")
+			os.Exit(0)
+		}
+
+		switch config["files to get"] {
+		case "latest":
+			lumberjack.Info("Getting latest file from matches")
+			sort.Strings(matchingFiles)
+			filesToDownload =
+				append(filesToDownload, matchingFiles[len(matchingFiles)-1])
+			lumberjack.Info("Latest file is %v", filesToDownload[0])
+		case "all":
+			lumberjack.Info("Getting all matching files")
+			filesToDownload = matchingFiles
+			lumberjack.Info("Files to download: %v", filesToDownload)
+		default:
+			lumberjack.Panic("Don't know which files to get; update config with 'latest' or 'all'")
+		}
+
+	} else {
+		filesToDownload =
+			append(filesToDownload, fmt.Sprintf("%v", config["download host"]))
+	}
+
+	if len(filesToDownload) == 0 {
+		lumberjack.Info("No files to download")
+	}
+
+	// Get files
+	for _, file := range filesToDownload {
+		downloadLink := fmt.Sprintf("%v", config["download root"]) + "/" + file
+		filePath := "./" + file
+		gatherData(downloadLink, filePath)
+	}
+}
+
+/* private */
+
+// Given a uri and a file path, get the data and save to the path
+func gatherData(downloadLink string, filePath string) error {
+	lumberjack.Info("Getting response from " + downloadLink)
+
+	response, err := http.Get(downloadLink)
+	if err != nil {
+		lumberjack.Panic("Error in Get: %v", err)
+	}
+	defer response.Body.Close()
+
+	// Download file
+	lumberjack.Info("Starting to download " + downloadLink)
+
+	downloadHandle, err := os.Create(filePath)
+	if err != nil {
+		lumberjack.Panic("Error creating file %v", err)
+	}
+	defer downloadHandle.Close()
+
+	_, err = io.Copy(downloadHandle, response.Body)
+	if err != nil {
+		lumberjack.Error("Failed to download file %v", downloadLink)
+	}
+
+	lumberjack.Info("Finished downloading %v", downloadLink)
+	return err
+}
+
+func getFileList(config map[string]interface{}) (files []string, err error) {
 	// Get response from host
-	lumberjack.Info("Calling Get from %v", config["download host"])
+	lumberjack.Info("Calling Get on %v", config["file list host"])
 
-	response, err := http.Get(fmt.Sprintf("%v", config["download host"]))
+	response, err := http.Get(fmt.Sprintf("%v", config["file list host"]))
 	if err != nil {
 		lumberjack.Panic("Error in Get: ", err)
 	}
 	defer response.Body.Close()
-
-	// Check response
-	if response.StatusCode != 200 {
-		lumberjack.Fatal("Failed to get 200 status")
-	}
 
 	// Parse response
 	fileRegexp := regexp.MustCompile(fmt.Sprintf("%v", config["file pattern"]))
@@ -56,34 +125,31 @@ func main() {
 		lumberjack.Panic("Error in scanning response: ", err)
 	}
 
-	// Get latest file
-	sort.Strings(matchingFiles)
-	latestFile := matchingFiles[len(matchingFiles)-1]
-	lumberjack.Info("Latest file is %v", latestFile)
+	return matchingFiles, err
+}
 
-	// Get file
-	downloadLink := fmt.Sprintf("%v", config["download host"]) + "/" + latestFile
-	lumberjack.Info("Calling Get from " + downloadLink)
+// GIven a JSON config file name, read in the file and return a go lang
+// data structure
+func readConfig(fileName string, theTime time.Time) map[string]interface{} {
+	lumberjack.Info("Reading in config " + fileName)
 
-	response, err = http.Get(downloadLink)
+	contents, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		lumberjack.Panic("Error in Get: %v", err)
-	}
-	defer response.Body.Close()
-
-	// Download file
-	lumberjack.Info("Starting download of " + downloadLink)
-
-	downloadHandle, err := os.Create("./" + latestFile)
-	if err != nil {
-		lumberjack.Panic("Error creating file %v", err)
-	}
-	defer downloadHandle.Close()
-
-	_, err = io.Copy(downloadHandle, response.Body)
-	if err != nil {
-		lumberjack.Error("Failed to download file %v", downloadLink)
+		lumberjack.Fatal("Couldn't read your config file.  Contents: %v", contents)
 	}
 
-	lumberjack.Info("Finished downloading %v", downloadLink)
+	lumberjack.Info("Replacing time vars in config")
+
+	replacedContents :=
+		timepiece.ReplaceTime(string(contents), timepiece.TimeToTimePiece(theTime))
+
+	lumberjack.Info("Unmarshalling config from JSON")
+
+	var configJson interface{}
+	err = json.Unmarshal([]byte(replacedContents), &configJson)
+	if err != nil {
+		lumberjack.Fatal("Failed to Unmarshal JSON; contents: %v", configJson)
+	}
+
+	return configJson.(map[string]interface{})
 }
